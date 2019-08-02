e14e8b53e08813ef5c7df774f1c3d2747f5bfd16
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-08-02 18:42:16 +0300

    SKALE-1396 Retrieve require messages in separate file

---
+++ test/utils/constants.js
@@ -0,0 +1,13 @@
+const filestorageRequireMessages = {
+    FILE_NOT_FOUND: 'File not found',
+    FILE_NOT_EXISTS: 'File not exists',
+    INVALID_PATH: 'Incorrect file path',
+    NOT_EMPTY_DIRECTORY: 'Directory is not empty',
+    FILE_ALREADY_EXISTS: 'File already exists',
+    INCORRECT_FILENAME: 'Filename should be <= 256 and not contains \'/\'',
+    INCORRECT_FILESIZE: 'File should be less than 100 MB',
+    NOT_ENOUGH_FREE_SPACE: 'Not enough free space in the Filestorage',
+    INCORRECT_CHUNK_POSITION: 'Incorrect position of chunk',
+    INCORRECT_CHUNK_LENGTH: 'Incorrect chunk length',
+    CHUNK_ALREADY_UPLOADED: 'Chunk is already uploaded',
+};
