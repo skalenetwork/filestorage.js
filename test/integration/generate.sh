3a32cd642669431f5167aac4030ef8cac2e45203
EvgeniyZZ <evgeniy@skalelabs.com>
2019-09-20 16:38:13 +0300

    SKALE-1487 Update dir path

--- test/integration/generate.sh
+++ test/integration/generate.sh
@@ -6,15 +6,15 @@ then
     mkdir -p ./testFiles/small
     mkdir -p ./testFiles/large
 
-    for n in {1..3}; do
+    for n in {1..3000}; do
         dd if=/dev/urandom of=./testFiles/tiny/tiny$( printf %00d "$n" ).txt bs=1024 count=1
     done
 
-    for n in {1..3}; do
+    for n in {1..3000}; do
         dd if=/dev/urandom of=./testFiles/small/small$( printf %00d "$n" ).txt bs=1024 count=1024
     done
 
-    for n in {1..14}; do
+    for n in {1..50}; do
         dd if=/dev/urandom of=./testFiles/large/large$( printf %00d "$n" ).txt bs=1047552 count=94
     done
 fi
