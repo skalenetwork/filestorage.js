91f6e3595b0c37d55251b48105cce652b841b0e7
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-04-17 14:17:37 +0300

    SKALE-2446 Retrieve port

--- run_test_container.sh
+++ run_test_container.sh
@@ -1,14 +1,11 @@
-port=$((1000 + RANDOM % 10000)); 
+ssh -o StrictHostKeyChecking=no $USER@$ENDPOINT "mkdir schains/$PORT"
+ssh -o StrictHostKeyChecking=no $USER@$ENDPOINT "mkdir schains/$PORT/data_dir"
 
-ssh -o StrictHostKeyChecking=no $USER@$ENDPOINT "mkdir schains/$port"
-ssh -o StrictHostKeyChecking=no $USER@$ENDPOINT "mkdir schains/$port/data_dir"
-
-scp -o StrictHostKeyChecking=no ./test/utils/config.json $USER@$ENDPOINT:schains/$port/config.json > /dev/null
-ssh -o StrictHostKeyChecking=no $USER@$ENDPOINT "docker run -d -v ~/schains/$port:/schain_data \
-                    -p $port:2234 \
+scp -o StrictHostKeyChecking=no ./test/utils/config.json $USER@$ENDPOINT:schains/$PORT/config.json
+ssh -o StrictHostKeyChecking=no $USER@$ENDPOINT "docker run -d -v ~/schains/$PORT:/schain_data \
+                    -p $PORT:2234 \
                     -e SSL_CERT_PATH=None \
                     -e HTTP_RPC_PORT=2234 \
                     -e DATA_DIR=/schain_data/data_dir \
                     -e CONFIG_FILE=/schain_data/config.json \
-                    skalenetwork/schain:$VERSION" > /dev/null
-echo $port
+                    skalenetwork/schain:$VERSION"
