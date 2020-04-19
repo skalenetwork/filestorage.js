862c07cd9784dd7789ab525a147e346541d00ce1
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2020-04-19 22:59:42 +0300

    SKALE-2446 Fix codacy

--- scripts/run_remote_schain.sh
+++ scripts/run_remote_schain.sh
@@ -3,7 +3,7 @@
 ssh -o StrictHostKeyChecking=no "$USER"@"$ENDPOINT" "mkdir schains/$PORT"
 ssh -o StrictHostKeyChecking=no "$USER"@"$ENDPOINT" "mkdir schains/$PORT/data_dir"
 
-scp -o StrictHostKeyChecking=no ./test/utils/config.json "$USER"@"$ENDPOINT":schains/$PORT/config.json
+scp -o StrictHostKeyChecking=no ./test/utils/config.json "$USER"@"$ENDPOINT":schains/"$PORT"/config.json
 ssh -o StrictHostKeyChecking=no "$USER"@"$ENDPOINT" "docker run -d -v ~/schains/$PORT:/schain_data \
                     -p $PORT:2234 \
                     -e SSL_CERT_PATH=None \
