43c3515b8cb2cf2fe5d3296aeac5f6b314ce0e13
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-05-18 18:14:00 +0300

    SKALE-1487 Update generate.sh

--- test/integration/generate.sh
+++ test/integration/generate.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
-if [ ! -d ./testFiles/ ]
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ ! -d $CUR_DIR/testFiles/ ]
 then
-    mkdir -p ./testFiles/
-    mkdir -p ./testFiles/tiny
-    mkdir -p ./testFiles/small
-    mkdir -p ./testFiles/large
+    mkdir -p $CUR_DIR/testFiles/
+    mkdir -p $CUR_DIR/testFiles/tiny
+    mkdir -p $CUR_DIR/testFiles/small
+    mkdir -p $CUR_DIR/testFiles/large
 
     for n in {1..3000}; do
-        dd if=/dev/urandom of=./testFiles/tiny/tiny$( printf %00d "$n" ).txt bs=1024 count=1
+        dd if=/dev/urandom of=$CUR_DIR/testFiles/tiny/tiny$( printf %00d "$n" ).txt bs=1024 count=1
     done
 
     for n in {1..3000}; do
-        dd if=/dev/urandom of=./testFiles/small/small$( printf %00d "$n" ).txt bs=1024 count=1024
+        dd if=/dev/urandom of=$CUR_DIR/testFiles/small/small$( printf %00d "$n" ).txt bs=1024 count=1024
     done
 
     for n in {1..50}; do
-        dd if=/dev/urandom of=./testFiles/large/large$( printf %00d "$n" ).txt bs=1047552 count=94
+        dd if=/dev/urandom of=$CUR_DIR/testFiles/large/large$( printf %00d "$n" ).txt bs=1047552 count=94
     done
 fi
 
