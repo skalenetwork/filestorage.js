ff0e19ba8baae9fc6e078151668bdca04d59644b
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-05-18 19:15:20 +0300

    SKALE-3904 Get address from pk

--- test/integration/test.js
+++ test/integration/test.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const Filestorage = require('../../src/index');
+const getAddress = require('../utils/helper').getAddress;
 
 let Web3 = require('web3');
 let fs = require('fs')
 let pk = process.env.PK;
 let endpoint = process.env.SCHAIN_ENDPOINT;
 let type = process.env.TYPE || 'tiny';
+let address = getAddress(pk);
 let web3 = new Web3(endpoint);
-let filestorage = new Filestorage(endpoint, true);
-
+let filestorage = new Filestorage(endpoint);
 
 let fileNumber = {
     'tiny': 3000,
@@ -17,18 +18,19 @@ let fileNumber = {
 }
 
 async function upload(){
-    let balanceStart = await web3.eth.getBalance(process.env.ACC1)
-    let timeStart = new Date()
-    console.log(balanceStart)
-    console.log('timeStart: ', timeStart)
+    console.log(type);
+    let balanceStart = await web3.eth.getBalance(address);
+    let timeStart = new Date();
+    console.log(balanceStart);
+    console.log('timeStart: ', timeStart);
     for (let i = 1; i < fileNumber[type] ; ++i) {
-        let directoryPath = __dirname+'/testFiles/'+type+'/'+type+i+'.txt'
+        let directoryPath = __dirname+'/testFiles/'+type+'/'+type+i+'.txt';
         var content;
         var contentPath;
         content = await fs.readFileSync(directoryPath)
         contentPath = await filestorage.uploadFile(address,type+i+'.txt', content,  pk);
         console.log(contentPath);
-        let balanceFinish = await web3.eth.getBalance(process.env.ACC1)
+        let balanceFinish = await web3.eth.getBalance(address)
         let balanceFinishEth = await web3.utils.fromWei((balanceStart - balanceFinish).toString(),"ether")
         let receipt = contentPath.txs[0];
         console.log('contentPath ', receipt.contentPath)
@@ -42,5 +44,3 @@ async function upload(){
 }
 
 upload();
-
-
