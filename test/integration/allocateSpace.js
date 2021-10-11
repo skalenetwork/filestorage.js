7350c474710b38428004e4cdab42ccaee63e4fc4
Oleksandr <oleksandr@skalelabs.com>
2021-10-11 15:16:58 +0300

    SKALE-4647-update allocate space tool

--- test/integration/allocateSpace.js
+++ test/integration/allocateSpace.js
@@ -4,15 +4,15 @@ const getAddress = require('../utils/helper').getAddress;
 
 let endpoint = process.env.SCHAIN_ENDPOINT;
 let pk = process.env.PK;
-let userAddress = process.env.USER_ADDRESS;
+let addressToReserve = process.env.USER_ADDRESS;
 
-const reservedSpace = 3 * 10 ** 8;
+const reservedSpace =  3 * 10 ** 8;
 
 let ownerAddress = getAddress(pk);
 let filestorage = new Filestorage(endpoint, true);
 
 async function allocateSpace() {
-    await filestorage.reserveSpace(ownerAddress, userAddress, reservedSpace, pk);
+    await filestorage.reserveSpace(ownerAddress, addressToReserve, reservedSpace, pk);
 }
 
 allocateSpace();
